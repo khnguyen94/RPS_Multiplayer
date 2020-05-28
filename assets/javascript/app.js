95a06e144b4f45bb14c95d0fd0d0b3d2094014fb
Khoa Nguyen <kh.nguyen0101@gmail.com>
2020-05-27 20:22:35 -0700

    initialized firebase in app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDD9_RFpr7ENcB-oJ8uUjge2T8OOgtcG58",
+  authDomain: "rps-multiplayer-9e346.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-9e346.firebaseio.com",
+  projectId: "rps-multiplayer-9e346",
+  storageBucket: "rps-multiplayer-9e346.appspot.com",
+  messagingSenderId: "567696946751",
+  appId: "1:567696946751:web:c47ca8c4496ef268096a8a",
+  measurementId: "G-8PQZ9VBD65",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Create variables to reference different parts of firebase data storage
+var database = firebase.database();
+// let chatData = firebase.ref("/chat");
+// let playersRef = database.ref("players");
+// let currentTurnRef = database.ref("turn");
+
+// Initialize game variables
+let username = "Guest";
+let currentPlayers = null;
+let currentTurn = null;
+let playerNum = false;
+let playerOneExists = false;
+let playerTwoExists = false;
+let playerOneData = null;
+let playerTwoData = null;
+
+// Create HTML Dom references
+let loginBtn = $("#login-btn");
+let usernameText = $("username-text");
+
+// Create a function to listen to user logging in
+// On click of the loginBtn, run a function
+loginBtn.click(function () {
+  // That checks to see if the usernameText value is not equal to an empty string
+  if (usernameText.val() !== "") {
+    console.log(usernameText.val());
+    // If so, set the username variable to be the value the user entered, capitalized
+    username = usernameText.val();
+  }
+});
